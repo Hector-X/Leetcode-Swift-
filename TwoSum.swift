0fc7070936ecc79bf0a09d6a4bb7f1be1c800490
徐坚 <xujianhust@outlook.com>
2017-09-27 16:15:23 +0800

    1

--- TwoSum.swift
+++ TwoSum.swift
@@ -1,12 +1,13 @@
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     var resultArray:[Int] = []
-    var numsMap:[Int:Int] = [:]
-    for (index,num) in nums.enumerated() {
+    var numsMap:[Int:Int] = [:] //hash表
+    for (index,num) in nums.enumerated() {//swift采用这种方式同时遍历容器的Object和index
         numsMap[num] = index
     }
     for (index,num) in nums.enumerated() {
         let diff = target - num;
-        if (numsMap[diff] != nil) && (numsMap[diff]!>index) {
+        if (numsMap[diff] != nil) && (numsMap[diff]!>index) {//在hash表中查找元素直接用key访问该元素即可，
+                                                             //不为nil即该元素存在
             resultArray.append(index)
             resultArray.append(numsMap[diff]!)
         }
